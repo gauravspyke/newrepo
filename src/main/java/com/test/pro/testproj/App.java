321f7872e3a9f8592d1d533241387b732aeeeebf
Gaurav Pahwa <gaurav.pahwa@ARICENT.COM>
2018-11-30 11:57:52 +0530

    version 1.3

--- src/main/java/com/test/pro/testproj/App.java
+++ src/main/java/com/test/pro/testproj/App.java
@@ -1,15 +1,33 @@
 package com.test.pro.testproj;
 
+
+
+class ABC {
+	
+	void anonymous(int a, int b){
+		
+		int c = a + b;
+		System.out.println(c);
+	}
+	
+	
+	
+}
+
 /**
  * Hello world!
  *
  */ 
 public class App 
 {
+	
+	
 	/**
 	* First comment
 	**/
-
+	static int abc = 10;
+	
+	
     public static void main_temp( String[] args )
     {
         System.out.println( "Hello World!1" );
