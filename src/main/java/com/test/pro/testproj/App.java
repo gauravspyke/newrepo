b4a05a4869e6d48e07571c27538712d33ce5a577
Gaurav Pahwa <gaurav.pahwa@ARICENT.COM>
2018-11-30 11:52:05 +0530

    version 1.1

--- src/main/java/com/test/pro/testproj/App.java
+++ src/main/java/com/test/pro/testproj/App.java
@@ -3,37 +3,15 @@ package com.test.pro.testproj;
 /**
  * Hello world!
  *
- */ ABC
+ */ 
 public class App 
 {
 	/**
-	* Yahooo comment
+	* First comment
 	**/
 
     public static void main_temp( String[] args )
     {
-		print
         System.out.println( "Hello World!1" );
     }
-
-	
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!1" );
-    }
-	
-	print("Debug")
-	
-	
-    public static void main_temp( String[] args )
-    {
-        System.out.println( "Hello World!1" );
-    }
-
-	
-    public static void main( String[] args )
-    {
-		dsfsdf
-        System.out.println( "Hello World!1" );
-    }	
 }
