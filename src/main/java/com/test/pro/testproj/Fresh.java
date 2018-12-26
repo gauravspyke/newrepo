d2a2c30d4ae527ece00f42d34cfc15b0e7ea44ec
Gaurav Pahwa <gaurav.pahwa@ARICENT.COM>
2018-12-26 16:54:07 +0530

    version 2.7 Deletion, additon and modification

--- src/main/java/com/test/pro/testproj/Fresh.java
+++ src/main/java/com/test/pro/testproj/Fresh.java
@@ -3,24 +3,26 @@ package com.test.pro.testproj;
 
 /**
  * Test Fresh Class
- *
  */ 
 public class App 
 {
 	
 	/**
+	*
 	* First comment
 	**/
     public static void main_temp( String[] args )
     {
-		System.out.println( "Hello World 2 !!!" );
+		System.out.println( "Hello World 1 !!!" );
+		System.out.println( "Hello World 4 !!!" );
     }
 	
 	public void test(String abc){
 		int a = 11;
 		int b = 20;
 		int sumnew  = a + b;
-		int finalsum = sumnew + 12;		
+		int finalsum = sumnew + 12;	
+		/* nothing */
 		System.out.println(finalsum);
 		System.out.println(sum);
 		
