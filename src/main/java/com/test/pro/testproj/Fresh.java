6538effe2691a1cafa1481d8002c371c9f1b5a0c
Gaurav Pahwa <gaurav.pahwa@ARICENT.COM>
2018-12-26 13:09:59 +0530

    version 2.6 only deletions

--- src/main/java/com/test/pro/testproj/Fresh.java
+++ src/main/java/com/test/pro/testproj/Fresh.java
@@ -13,14 +13,12 @@ public class App
 	**/
     public static void main_temp( String[] args )
     {
-        System.out.println( "Hello World!!!!" );
 		System.out.println( "Hello World 2 !!!" );
     }
 	
 	public void test(String abc){
 		int a = 11;
 		int b = 20;
-		int sum = a - b ;
 		int sumnew  = a + b;
 		int finalsum = sumnew + 12;		
 		System.out.println(finalsum);
