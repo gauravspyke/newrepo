11bd2137ab5519e6e863e1f666d2e2e420cbdcaf
Gaurav Pahwa <gaurav.pahwa@ARICENT.COM>
2018-12-26 11:27:47 +0530

    version 2.1

---
+++ src/main/java/com/test/pro/testproj/Fresh.java
@@ -0,0 +1,25 @@
+package com.test.pro.testproj;
+
+
+/**
+ * Test Fresh Class
+ *
+ */ 
+public class App 
+{
+	
+	/**
+	* First comment
+	**/
+    public static void main_temp( String[] args )
+    {
+        System.out.println( "Hello World!!!!" );
+    }
+	
+	public void test(String abc){
+		int a = 10;
+		int b = 20;
+		int sum = a - b ;
+		System.out.println(sum);
+	}
+}
